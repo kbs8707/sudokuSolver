2e9c04f350d67bef6ddfd5914d682a6e81e77753
John Kang <kbs8707@hotmail.com>
2021-03-29 00:13:42 -0400

    Fixed remove from possibleMoves[][]

--- sudokuSolver.py
+++ sudokuSolver.py
@@ -1,9 +1,39 @@
+import copy
+
 def solve(board):
+    currentBoard = copy.deepcopy(board)
     possibleMoves = [[[] for i in range(len(board))] for j in range(len(board[0]))]
 
-    checkValid(board, possibleMoves)
+    #Set determinant moves, this will reduce the number of moves for the backtracking algorithm
+    checkValid(currentBoard, possibleMoves, True)
     
-def checkValid(board, possibleMoves):
+    #Backtracking algorithm
+    backTrack(currentBoard, possibleMoves)
+
+    #Print the initial board
+    print("Input Board:")
+    printBoard(board)
+    #Print the solved board
+    print("Solved Board:")
+    printBoard(currentBoard)
+
+
+def checkValid(board, possibleMoves, firstRun):
+
+    #Exit condition, exit if there are no determinant moves available, backtracking algorithm is needed next
+    #Ensure the recursion doesn't break on an empty list the first time this function runs, if possibleMoves[][] is empty after this runs then the board is solved
+    # emptyMoves = True
+    # for i in range(len(board)):
+    #     for j in range(len(board[i])):
+    #         if (len(possibleMoves[i][j]) != 0):
+    #             emptyMoves = False
+    breakFlag = False
+    for i in range(len(board)):
+        for j in range(len(board[i])):
+            if (len(possibleMoves[i][j]) == 1):
+                breakFlag = True
+    if (not breakFlag and not firstRun):
+        return board, possibleMoves
 
     #Set up all possible moves for every slot
     for i in range(len(board)):
@@ -19,20 +49,27 @@ def checkValid(board, possibleMoves):
             if (len(possibleMoves[i][j]) == 1):
                 #Update the board
                 board[i][j] = possibleMoves[i][j][0]
-                #Remove the move in the possible move list from other slots on the same row, column, and sector
+
+                #Remove the move in the possibleMoves[][] from other slots on the same row, column, and sector
                 for k in range(len(board[i])):
-                    if (board[i][k] != 0 and board[i][k] in possibleMoves[i][j]):
-                        possibleMoves[i][j].remove(board[i][k])
-
+                    # if (board[i][k] != 0 and board[i][j] in possibleMoves[i][k]):
+                    if (board[i][j] in possibleMoves[i][k]):
+                        possibleMoves[i][k].remove(board[i][j])
                 for l in range(len(board[0])):
-                    if (board[l][j] != 0 and board[l][j] in possibleMoves[i][j]):
-                        possibleMoves[i][j].remove(board[l][j])
+                    # if (board[l][j] != 0 and board[i][j] in possibleMoves[l][j]):
+                    if (board[i][j] in possibleMoves[l][j]):
+                        possibleMoves[l][j].remove(board[i][j])
+                sector_x = j // 3
+                sector_y = i // 3
+                for m in range(sector_y*3, sector_y*3+3):
+                    for n in range(sector_x*3, sector_x*3+3):
+                        # if (board[m][n] != 0 and board[i][j] in possibleMoves[m][n]):
+                        if (board[i][j] in possibleMoves[m][n]):
+                            possibleMoves[m][n].remove(board[i][j])
 
-    checkValid(board, possibleMoves)
 
-    #Backtracking algorithm
-
-    return
+    #Runs recursively until all determinant moves are placed
+    checkValid(board, possibleMoves, False)
 
 def checkRow(board, row):
     validMove = [1,2,3,4,5,6,7,8,9]
@@ -60,6 +97,22 @@ def checkSector(board, row, col):
 
     return validMove
 
+#TODO
+def backTrack(board, possibleMoves):
+    pass
+
+#Prints the board in a presentable way
+def printBoard(board):
+    print("-"*37)
+    for i, row in enumerate(board):
+        print(("|" + " {}   {}   {} |"*3).format(*[x if x != 0 else " " for x in row]))
+        if i == 8:
+            print("-"*37)
+        elif i % 3 == 2:
+            print("|" + "---+"*8 + "---|")
+        else:
+            print("|" + "   +"*8 + "   |")
+
 board = [
     [7,8,0,4,0,0,1,2,0],
     [6,0,0,0,7,5,0,0,9],
@@ -84,4 +137,4 @@ board = [
 #     [0,5,4,0,0,8,6,7,2]
 # ]
 
-print(solve(board))
+solve(board)
